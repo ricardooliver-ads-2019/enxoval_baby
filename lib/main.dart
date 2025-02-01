8acfc27ce881e7425dfd8703e874d63ca9a37509
RICARDO OLIVEIRA <ricardooliver.ads.2019@gmail.com>
2025-02-01 16:39:41 -0400

    Revert "chore(firebase)Adiciona as configurações do projeto para acessar o firebase"
    This reverts commit 2d6d197dbe4d0aebc9d2c852db8cb3de483de273.

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,6 @@
 import 'package:enxoval_baby/app/enxoval_baby_app.dart';
-import 'package:enxoval_baby/firebase_options.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+void main() {
   runApp(const EnxovalBabyApp());
 }
